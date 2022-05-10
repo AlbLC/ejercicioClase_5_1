499e7556fe94c23831c478ae2c03abcbd2bf18f7
bogdaniam <mihai.bogdan.mardale@gmail.com>
2022-05-10 19:19:17 +0200

    El segundo commit

--- script.js
+++ script.js
@@ -27,19 +27,19 @@ window.onload = function () {
 //Comprobar la primera letra del nombre
 function soloLetras1() {
     if ((event.keyCode < 65) || (event.keyCode > 90) && (event.keyCode < 97) || (event.keyCode > 122))
-       event.returnValue = false;
-       var nombre = document.getElementById("nombre").value;
+        event.returnValue = false;
+    var nombre = document.getElementById("nombre").value;
     if ((nombre[0].charCodeAt() >= 65) && (nombre[0].charCodeAt() <= 90)) {
         document.getElementById("pnombre").innerText = "Nombre OK"
-      } else {
+    } else {
         document.getElementById("pnombre").innerText = "La primera letra del nombre tiene que ser mayuscula"
-      }
     }
+}
 
 //Comprobar la primera letra del primer apellido 
 function soloLetras2() {
     if ((event.keyCode < 65) || (event.keyCode > 90) && (event.keyCode < 97) || (event.keyCode > 122))
-       event.returnValue = false;
+        event.returnValue = false;
     var apellido1 = document.getElementById("apellido1").value;
     if ((apellido1[0].charCodeAt() >= 65) && (apellido1[0].charCodeAt() <= 90)) {
         document.getElementById("papellido1").innerText = "Apellido OK"
@@ -51,7 +51,7 @@ function soloLetras2() {
 //Comprobar la primera letra del segundo apellido 
 function soloLetras3() {
     if ((event.keyCode < 65) || (event.keyCode > 90) && (event.keyCode < 97) || (event.keyCode > 122))
-       event.returnValue = false;
+        event.returnValue = false;
     var apellido2 = document.getElementById("apellido2").value;
     if ((apellido2[0].charCodeAt() >= 65) && (apellido2[0].charCodeAt() <= 90)) {
         document.getElementById("papellido2").innerText = "Apellido OK"
@@ -61,9 +61,9 @@ function soloLetras3() {
 }
 
 
-    
 
-    
+
+
 
 
 
@@ -72,34 +72,49 @@ function emailValidar() {
     var email = document.getElementById("email").value;
     var email1 = document.getElementById("email1").value;
 
-//Termino que introduce el usuario
-var termBorrar = document.getElementById("term").value
-//Creamos la lista con todos los DT
-var listaDT = document.querySelectorAll("dt")
-//Creamos la lista con todos los DT
-var listaDD = document.querySelectorAll("dd")
+    //COmprobar los email entre si
+    var contador = 0;
+    if (email.length == email1.length) {
+        for (let i = 0; i < email.length; i++) {
 
-    for (let i = 0; i < email.length; i++){
+            if (email[i] != email1[i]) {
+                i = email.length;
+                console.log("Los email introducidos no coinciden");
 
-        //Utilizamos la condicion if para comprobar si el innerText de cada elemento de la listaDT conincide con el termBorrar (termino introducido por el usuario)
-        if(listaDT[i].innerText == termBorrar ) {
-            //Cuando la condicion se cumple, se borra el elemento de la lista DT mas elelemento de la lista DD
-            listaDT[i].parentNode.removeChild(listaDT[i]);
-            listaDD[i].parentNode.removeChild(listaDD[i]);
-            //Se utiliza para parrar el for
-            i = listaDT.length
-        }  
+            } else {
+                contador++;
+            }
+            if (contador == email.length) {
+                console.log("Los email introducidos coinciden");
+            }
+        }
+    } else {
+        console.log("11Los email introducidos no coinciden");
     }
+
 
 }
 
+//COmprobar la validez del email
+let antesArroba=[];
+let contador1=0;
+for (let i = 0; i < email.length; i++) {
+    if (email[i] == '@') {
+       antesArroba= email.slice(0,i);
 
-
-
+    } else {
+        contador1 ++;
+    }
+}
+console.log(antesArroba)
+console.log(contador1)
+if(contador1 == email.length) {
+    console.log('falta el @')
+}
 
 function registrado() {
 
-   
+
 
 
 
